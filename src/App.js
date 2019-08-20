93c43f849abcb17c93fdbd4a849ca98dd6206814
twkim <shelajad.dev@gmail.com>
2019-08-20 21:44:42 +0900

    JSX and Props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types'
+
+const foodList = [
+  {
+    id: 1,
+    name: "kimchi",
+    img: "http://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
+    rating: "5.0"
+  },
+  {
+    id: 2,
+    name: "ramen",
+    img: "http://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
+    rating: "3.9"
+  },
+  {
+    id: 3,
+    name: "gogi",
+    img: "http://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
+    rating: "4.5"
+  }
+];
+
+
+function Food({ name, picture, rating }) {
+  return <div>
+    <h1> I like {name} </h1>
+    <h4>{rating}/5.0</h4>
+    <img src={picture} alt={name} />
+  </div>
+}
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.string.isRequired
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>
+    {foodList.map(dish => <Food key={dish.id} name={dish.name} picture={dish.img} rating={dish.rating} />)}
+  </div >;
 }
 
 export default App;
