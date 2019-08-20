603bf391b293ed88249d5a6e1595443db1abe3e3
twkim <shelajad.dev@gmail.com>
2019-08-20 23:48:04 +0900

    STATE.... counting

--- src/App.js
+++ src/App.js
@@ -1,46 +1,25 @@
 import React from 'react';
-import PropTypes from 'prop-types'
 
-const foodList = [
-  {
-    id: 1,
-    name: "kimchi",
-    img: "http://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-    rating: "5.0"
-  },
-  {
-    id: 2,
-    name: "ramen",
-    img: "http://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-    rating: "3.9"
-  },
-  {
-    id: 3,
-    name: "gogi",
-    img: "http://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-    rating: "4.5"
+class App extends React.Component {
+  state = {
+    count: 0
   }
-];
 
-
-function Food({ name, picture, rating }) {
-  return <div>
-    <h1> I like {name} </h1>
-    <h4>{rating}/5.0</h4>
-    <img src={picture} alt={name} />
-  </div>
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.string.isRequired
-}
-
-function App() {
-  return <div>
-    {foodList.map(dish => <Food key={dish.id} name={dish.name} picture={dish.img} rating={dish.rating} />)}
-  </div >;
+  add = () => {
+    this.setState(current => ({ count: current.count + 1 }));
+  }
+  minus = () => {
+    this.setState(current => ({ count: current.count - 1 }));
+  }
+  render() {
+    return (
+      <div>
+        <h1> The number is : {this.state.count} </h1>
+        <button onClick={this.add}>add</button>
+        <button onClick={this.minus}>minus</button>
+      </div>
+    )
+  }
 }
 
 export default App;
