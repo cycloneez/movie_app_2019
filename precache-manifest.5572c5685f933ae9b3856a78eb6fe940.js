2b592ab575386914d25820d8215db7d0923a5ff4
twkim <shelajad.dev@gmail.com>
2019-08-21 23:35:06 +0900

    Updates

---
+++ precache-manifest.5572c5685f933ae9b3856a78eb6fe940.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82fa233756072f5aa49714871fee3dc5",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "ae6a2c812a4610e80b92",
+    "url": "/movie_app_2019/static/css/main.f1ddd916.chunk.css"
+  },
+  {
+    "revision": "3350fcf69a8fdf7d4a1c",
+    "url": "/movie_app_2019/static/js/2.1c4cb735.chunk.js"
+  },
+  {
+    "revision": "ae6a2c812a4610e80b92",
+    "url": "/movie_app_2019/static/js/main.180713af.chunk.js"
+  },
+  {
+    "revision": "8e34f0f3f9415f236696",
+    "url": "/movie_app_2019/static/js/runtime~main.97c70ab9.js"
+  }
+]);
